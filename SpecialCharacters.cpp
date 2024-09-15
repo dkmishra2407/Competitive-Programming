222cfc977e055bda8e9e6843e74ac81e8e9e5b75
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2024-09-14 19:37:15 -0700

    First Commit

--- SpecialCharacters.cpp
+++ SpecialCharacters.cpp
@@ -1,19 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+ 
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin>>n;
-        
-        if(n==1){
-            cout << "NO" << endl;
-            continue;
-        }
-
-        
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    if (n % 2 == 1) {
+      cout << "NO" << '\n';
+      continue;
     }
-    return 0;
+    cout << "YES" << '\n';
+    for (int i = 0; i < n / 2; ++i)
+      for (int j = 0; j < 2; ++j)
+        cout << "AB"[i & 1];
+    cout << '\n';
+  }
 }
